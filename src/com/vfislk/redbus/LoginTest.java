aa0fe9e143230e841bb55646955a5c1a4b8df186
balaji-githubstore <balaji0017@gmail.com>
2021-07-19 13:59:00 +0530

    chrome,firefox launch, basic activites, frame, exceptions - nosuchelement, elementnotinteractable, elementintercepted exception

---
+++ src/com/vfislk/redbus/LoginTest.java
@@ -0,0 +1,30 @@
+package com.vfislk.redbus;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LoginTest {
+
+	public static void main(String[] args) {
+		WebDriver driver=new ChromeDriver();	
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		
+		driver.get("https://www.redbus.in/");
+		
+		driver.findElement(By.id("i-icon-profile")).click();
+		driver.findElement(By.xpath("//li[text()='Sign In/Sign Up']")).click();
+		
+		driver.switchTo().frame(driver.findElement(By.xpath("//iframe[@class='modalIframe']")));
+		
+		
+		driver.findElement(By.id("mobileNoInp")).sendKeys("98989888");
+		//other operations
+		
+		driver.switchTo().defaultContent();
+	}
+}
+
