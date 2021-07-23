d22efa6e4e0191aa466cd776d231b17806f45db6
balaji-githubstore <balaji0017@gmail.com>
2021-07-23 11:18:36 +0530

    chromeoption

--- src/com/vfislk/seleniumadvance/MalaysiaAirlinesTest.java
+++ src/com/vfislk/seleniumadvance/MalaysiaAirlinesTest.java
@@ -8,22 +8,17 @@ import org.openqa.selenium.chrome.ChromeOptions;
 
 public class MalaysiaAirlinesTest {
 
-	public static void main(String[] args) {
-		
+	public static void main(String[] args) {	
 		ChromeOptions opt=new ChromeOptions();
 		opt.addArguments("--disable-notifications");
 	//	opt.setBinary("C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe");
 		
 		WebDriver driver=new ChromeDriver(opt);
-		
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 		
 		driver.get("https://www.malaysiaairlines.com/in/en.html");
 		
-		
-		
-
 	}
 
 }
